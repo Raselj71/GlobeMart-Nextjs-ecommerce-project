7daaf407ad5bef063da7a4564907bfc384e7ec5a
Raselj71 <raselj44@gmail.com>
2024-08-06 18:02:12 +0600

    changes settting

--- config/db.ts
+++ config/db.ts
@@ -1,15 +1,13 @@
-import mongoose,{Connection} from "mongoose";
+import mongoose from "mongoose";
 
-
-export async function connectdb(){
-     
-  try{
-    const conn=await mongoose.connect(String(process.env.MONGODB))
+export async function connectdb() {
+  try {
+    console.log("Connecting to MongoDB...");
+    const conn = await mongoose.connect(String(process.env.MONGODB));
+    console.log("Connected to MongoDB:", conn.connection.name);
     return conn;
-     
-  }catch(error){
-      console.log(error)
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+    throw error; // Ensure the error is rethrown for proper error handling
   }
-
 }
-
