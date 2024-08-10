f3216ee568545e9c1ecfc2828434179ab60762df
Raselj71 <raselj44@gmail.com>
2024-08-10 08:54:19 +0600

    Final Version

--- app/dashbord/profile/page.tsx
+++ app/dashbord/profile/page.tsx
@@ -1,7 +1,19 @@
-export default function page() {
+"use client";
+
+import { useSession } from "next-auth/react";
+
+const Page = () => {
+  const {data:session} = useSession();
+
   return (
-    <>
-      <div> i am profile page</div>
-    </>
+    <section className="w-full flex justify-center">
+      <div className="container">
+        <div className="text-xl font-semibold">Your Profile</div>
+        <div className="">{session?.user?.name}</div>
+        <div className="">{session?.user?.email}</div>
+      </div>
+    </section>
   );
-}
+};
+
+export default Page;
